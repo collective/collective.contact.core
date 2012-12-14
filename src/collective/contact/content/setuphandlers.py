8ccca11be773d8b4fbdf19fd2cd76bc9ce081ef1
Gauthier Bastien <g.bastien@imio.be>
2012-12-14 15:28:51 +0100

    Removed wrong command

--- src/collective/contact/content/setuphandlers.py
+++ src/collective/contact/content/setuphandlers.py
@@ -19,9 +19,8 @@ def postInstall(context):
     """Called as at the end of the setup process. """
     # the right place for your custom code
     if isNotCollectiveContactContentProfile(context): return
-    logStep("postInstall", context)
     site = context.getSite()
     # we CAN NOT reinstall the product using portal_quickinstaller because
     # it removes manualy added fields for dexterity types
-    #import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace()
     # we need to be sure 
