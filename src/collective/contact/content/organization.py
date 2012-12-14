6c1bcae009b7839eb4f8363c5148f1a9a8dbf337
Gauthier Bastien <g.bastien@imio.be>
2012-12-14 14:18:23 +0100

    Added policy

--- src/collective/contact/content/organization.py
+++ src/collective/contact/content/organization.py
@@ -3,6 +3,7 @@ from zope import schema
 
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from plone.supermodel.parser import ISchemaPolicy
 
 from . import _
 
@@ -18,3 +19,21 @@ class IOrganization(model.Schema):
 class Organization(Container):
     """ """
     implements(IOrganization)
+
+
+class OrganizationSchemaPolicy(object):
+    """ """
+    implements(ISchemaPolicy)
+    
+    def module(self, schemaName, tree):
+        return 'plone.dexterity.schema.transient'
+        
+    def bases(self, schemaName, tree):
+        return (IOrganization,)
+        
+    def name(self, schemaName, tree):
+        # We use a temporary name whilst the interface is being generated;
+        # when it's first used, we know the portal_type and site, and can
+        # thus update it
+        return '__tmp__' + schemaName
+
--- src/collective/contact/content/person.py
+++ src/collective/contact/content/person.py
@@ -47,3 +47,21 @@ class IPerson(model.Schema):
 class Person(Container):
     """ """
     implements(IPerson)
+
+
+class PersonSchemaPolicy(object):
+    """ """
+    implements(ISchemaPolicy)
+    
+    def module(self, schemaName, tree):
+        return 'plone.dexterity.schema.transient'
+        
+    def bases(self, schemaName, tree):
+        return (IPerson, )
+        
+    def name(self, schemaName, tree):
+        # We use a temporary name whilst the interface is being generated;
+        # when it's first used, we know the portal_type and site, and can
+        # thus update it
+        return '__tmp__' + schemaName
+
