50327b93a7a61b0181b2c62334db69cda767921c
Gauthier Bastien <g.bastien@imio.be>
2012-12-14 15:23:46 +0100

    Base on Dexterity Policy object, removed useless code...

--- src/collective/contact/content/organization.py
+++ src/collective/contact/content/organization.py
@@ -3,12 +3,13 @@ from zope import schema
 
 from plone.dexterity.content import Container
 from plone.supermodel import model
-from plone.supermodel.parser import ISchemaPolicy
+from plone.dexterity.schema import DexteritySchemaPolicy
 
 from . import _
 
 
 class IOrganization(model.Schema):
+    """ """
 
     organization_type = schema.Choice(
       title=_("Type"),
@@ -21,19 +22,9 @@ class Organization(Container):
     implements(IOrganization)
 
 
-class OrganizationSchemaPolicy(object):
+class OrganizationSchemaPolicy(DexteritySchemaPolicy):
     """ """
-    implements(ISchemaPolicy)
-    
-    def module(self, schemaName, tree):
-        return 'plone.dexterity.schema.transient'
-        
+
     def bases(self, schemaName, tree):
         return (IOrganization,)
-        
-    def name(self, schemaName, tree):
-        # We use a temporary name whilst the interface is being generated;
-        # when it's first used, we know the portal_type and site, and can
-        # thus update it
-        return '__tmp__' + schemaName
 
--- src/collective/contact/content/person.py
+++ src/collective/contact/content/person.py
@@ -2,14 +2,16 @@ from zope import schema
 from zope.interface import implements
 
 from plone.dexterity.content import Container
+from plone.dexterity.schema import DexteritySchemaPolicy
+
 from plone.namedfile.field import NamedImage
 from plone.supermodel import model
-from plone.supermodel.parser import ISchemaPolicy
 
 from . import _
 
 
 class IPerson(model.Schema):
+    """ """
 
     lastname = schema.TextLine(
         title=_(u"Lastname"),
@@ -49,19 +51,9 @@ class Person(Container):
     implements(IPerson)
 
 
-class PersonSchemaPolicy(object):
+class PersonSchemaPolicy(DexteritySchemaPolicy):
     """ """
-    implements(ISchemaPolicy)
-    
-    def module(self, schemaName, tree):
-        return 'plone.dexterity.schema.transient'
-        
+
     def bases(self, schemaName, tree):
         return (IPerson, )
-        
-    def name(self, schemaName, tree):
-        # We use a temporary name whilst the interface is being generated;
-        # when it's first used, we know the portal_type and site, and can
-        # thus update it
-        return '__tmp__' + schemaName
 
