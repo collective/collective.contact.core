c4b3775326ec0c72b338fb06844585434c48b1d6
Cédric Messiant <cedricmessiant@ecreall.com>
2012-12-24 16:36:02 +0100

    Compute person title from firstname and lastname

--- src/collective/contact/content/person.py
+++ src/collective/contact/content/person.py
@@ -1,6 +1,8 @@
 from zope import schema
 from zope.interface import implements
 
+from plone.app.content.interfaces import INameFromTitle
+
 from plone.dexterity.content import Container
 from plone.dexterity.schema import DexteritySchemaPolicy
 
@@ -8,6 +10,12 @@ from plone.namedfile.field import NamedImage
 from plone.supermodel import model
 
 from . import _
+
+
+class INameFromPersonNames(INameFromTitle):
+
+    def title(self):
+        """Return a processed title"""
 
 
 class IPerson(model.Schema):
@@ -18,32 +26,49 @@ class IPerson(model.Schema):
         required=True
         )
     gender = schema.Choice(
-       required=False,
-       values=("M", "F",),
+        title=_("Gender"),
+        values=("M", "F",),
+        required=False,
         )
     person_title = schema.TextLine(
-        required=False,
         title=_("Person title"),
+        required=False,
         )
     firstname = schema.TextLine(
-        required=False,
         title=_("Firstname"),
+        required=False,
         )
     lastname = schema.TextLine(
         title=_("Lastname")
         )
     birthday = schema.Date(
-        required=False,
         title=_("Birthday"),
+        required=False,
         )
     email = schema.TextLine(
-      required=False,
-      title=_("Email"),
+        title=_("Email"),
+        required=False,
         )
     photo = NamedImage(
-      required=False,
-      title=_("Photo"),
+        title=_("Photo"),
+        required=False,
         )
+
+
+class NameFromPersonNames(object):
+
+    implements(INameFromPersonNames)
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def title(self):
+        lastname = self.context.lastname
+        if self.context.firstname is not None and self.context.firstname:
+            return self.context.firstname + ' ' + lastname
+        else:
+            return lastname
 
 
 class Person(Container):
