7e47d1c305fb42bb3f04dde9fc9ce96891463890
Cédric Messiant <cedricmessiant@ecreall.com>
2012-12-31 15:50:12 +0100

    Fixes last commit (missing files)

---
+++ src/collective/contact/content/adapters.py
@@ -0,0 +1,92 @@
+from zope.interface import implements
+from zope.component import adapts
+
+from Products.CMFPlone.utils import base_hasattr
+
+import vobject
+
+from collective.contact.vcard.interfaces import IVCard
+from collective.contact.content.held_position import IHeldPosition
+
+
+def find_root_organization(position_or_organization):
+    for item in reversed(position_or_organization.aq_inner.aq_chain):
+        if base_hasattr(item, 'portal_type') and \
+           item.portal_type == 'organization':
+            return item
+    return ''
+
+
+class HeldPositionVCard(object):
+    implements(IHeldPosition)
+    adapts(IVCard)
+
+    def __init__(self, context):
+        self.context = context
+
+    def get_vcard(self):
+        vcard = vobject.vCard()
+        person = self.context.getParentNode()
+        vcard.add('n')
+        firstname = person.firstname or ''
+        lastname = person.lastname or ''
+        person_title = person.person_title
+        vcard.n.value = vobject.vcard.Name(prefix=person_title or '',
+                                           family=lastname,
+                                           given=firstname)
+        vcard.add('fn')
+        vcard.fn.value = '%s %s' % (firstname, lastname)
+
+        if person.birthday is not None:
+            vcard.add('bday')
+            vcard.bday.value = person.birthday.Date()
+
+        # held_position is linked to position or organization ?
+        position_or_organization = self.context.position
+        if position_or_organization.portal_type == 'position':
+            position_name = position_or_organization.Title()
+            vcard.add('role')
+            vcard.role.value = position_name
+            vcard.add('title')
+            vcard.title.value = position_name
+            organization = find_root_organization(position_or_organization)
+        elif position_or_organization.portal_type == 'organization':
+            organization = find_root_organization(position_or_organization)
+        vcard.add('org')
+        vcard.org.value = organization.Title()
+
+        if person.email is not None:
+            vcard.add('email')
+            vcard.email.type_param = 'INTERNET'
+            vcard.email.value = person.email
+
+        # if we don't have relevant address information, we don't need address
+        if person.city is not None or \
+           person.country is not None or \
+           person.region is not None:
+            vcard.add('adr')
+            vcard.adr.value = vobject.vcard.Address(street=person.street or '',
+                                                    city=person.city or '',
+                                                    region=person.region or '',
+                                                    code=person.zip_code or '',
+                                                    country=person.country or '',
+                                                    box=person.number or '',
+                                                    extended=person.additional_address_details or '')
+        if person.phone is not None:
+            vcard.add('tel')
+            vcard.tel.type_param = 'WORK'
+            vcard.tel.value = person.phone
+        if person.cell_phone is not None:
+            vcard.add('tel')
+            last_item = len(vcard.tel_list)-1
+            vcard.tel_list[last_item].type_param = 'CELL'
+            vcard.tel_list[last_item].value = person.cell_phone
+        #vcard.add('photo')
+        #vcard.photo.value = person.photo
+
+#        if person.latitude is not None and \
+#           person.longitude is not None:
+#            vcard.add('geo')
+#            vcard.geo.value = "%.2f;%.2f" % (person.latitude, person.longitude)
+
+        return vcard
---
+++ src/collective/contact/content/tests/test_adapters.py
@@ -0,0 +1,34 @@
+# -*- coding: utf8 -*-
+
+import unittest2 as unittest
+
+from ecreall.helpers.testing.base import BaseTest
+
+from collective.contact.content.testing import INTEGRATION
+from collective.contact.vcard.interfaces import IVCard
+
+
+class TestAdapters(unittest.TestCase, BaseTest):
+    """Tests adapters"""
+
+    layer = INTEGRATION
+
+    def setUp(self):
+        super(TestAdapters, self).setUp()
+        self.portal = self.layer['portal']
+        self.directory = self.portal['mydirectory']
+        self.degaulle = self.directory['degaulle']
+
+    def test_held_position_vcard(self):
+        gadt = self.degaulle['gadt']
+        vcard_provider = IVCard(gadt)
+        vcard = vcard_provider.get_vcard()
+        self.assertEqual(vcard.fn.value, 'Charles De Gaulle')
+        self.assertEqual(vcard.org.value, u'Armée de terre')
+        self.assertEqual(vcard.role.value, u'General of armée de terre')
+        self.assertEqual(vcard.title.value, u'General of armée de terre')
+        self.assertEqual(vcard.email.value, 'charles.de.gaulle@armees.fr')
+        self.assertEqual(vcard.email.type_param, 'INTERNET')
+        self.assertFalse(hasattr(vcard, 'adr'))
+        self.assertFalse(hasattr(vcard, 'tel_list'))
+        self.assertEqual(vcard.bday.value, '1890/11/22')
