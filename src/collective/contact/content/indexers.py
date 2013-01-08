2211bebeecc3a78c8503d949387a3bffc74a6c71
Cédric Messiant <cedricmessiant@ecreall.com>
2013-01-08 10:45:48 +0100

    Added indexer for HeldPosition

---
+++ src/collective/contact/content/indexers.py
@@ -0,0 +1,17 @@
+from plone.indexer import indexer
+
+from collective.contact.content.held_position import IHeldPosition
+
+
+@indexer(IHeldPosition)
+def held_position_searchable_text(obj):
+    indexed_fields = []
+    position_or_organization = obj.position.to_object
+    if position_or_organization.portal_type == 'position':
+        position = position_or_organization
+        indexed_fields.append(position.Title())
+        organization = position.getParentNode()
+    elif position_or_organization.portal_type == 'organization':
+        organization = position_or_organization
+    indexed_fields.extend(organization.get_full_title())
+    return ' '.join(indexed_fields)
