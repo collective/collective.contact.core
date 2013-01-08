fa8804d34aee1d975d25e0fad0e8a3c2a0036f3a
Cédric Messiant <cedricmessiant@ecreall.com>
2013-01-08 10:45:04 +0100

    Added get_full_title method for Organization

--- src/collective/contact/content/organization.py
+++ src/collective/contact/content/organization.py
@@ -1,11 +1,14 @@
 from zope.interface import implements
 from zope import schema
 
+from Acquisition import aq_inner, aq_chain
+
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from plone.dexterity.schema import DexteritySchemaPolicy
 
 from . import _
+from Products.CMFPlone.utils import base_hasattr
 
 
 class IOrganization(model.Schema):
@@ -20,6 +23,16 @@ class IOrganization(model.Schema):
 class Organization(Container):
     """ """
     implements(IOrganization)
+
+    def get_full_title(self):
+        full_title = []
+        for item in aq_chain(aq_inner(self)):
+            if base_hasattr(item, 'portal_type'):
+                if item.portal_type == 'directory':
+                    break
+                elif item.portal_type == 'organization':
+                    full_title.append(item.Title())
+        return full_title
 
 
 class OrganizationSchemaPolicy(DexteritySchemaPolicy):
