6087565a049d8bfb036b81c86c696a42e4e60640
Vincent Fretin <vincent.fretin@gmail.com>
2013-01-09 16:10:49 +0100

    added 'add new contact' link to the contact widget (in progress)

---
+++ src/collective/contact/content/addcontact.py
@@ -0,0 +1,39 @@
+from zope.i18nmessageid import MessageFactory
+
+from plone.autoform.form import AutoExtensibleForm
+from plone.autoform import directives as form
+import z3c.form
+from plone.supermodel import model
+
+from collective.contact.content.held_position import IHeldPosition
+from collective.contact.content.schema import ContactChoice
+from collective.contact.content.source import ContactSourceBinder
+from collective.contact.content.widgets import ContactAutocompleteFieldWidget
+
+_ = MessageFactory("collection.contact.content")
+
+
+class IAddContact(model.Schema):
+    organization = ContactChoice(
+            title=_(u"Organization"),
+            required=False,
+            source=ContactSourceBinder(portal_type="organization"))
+    form.widget(organization=ContactAutocompleteFieldWidget)
+
+    person = ContactChoice(
+            title=_(u"Person"),
+            required=False,
+            source=ContactSourceBinder(portal_type="person"))
+    form.widget(person=ContactAutocompleteFieldWidget)
+
+    position = ContactChoice(
+            title=_(u"Position"),
+            required=False,
+            source=ContactSourceBinder(portal_type="position"))
+    form.widget(person=ContactAutocompleteFieldWidget)
+
+
+class AddContact(AutoExtensibleForm, z3c.form.form.AddForm):
+    prefix = "addcontact"
+    schema = IAddContact
+    additionnalSchema = IHeldPosition
--- src/collective/contact/content/widgets.py
+++ src/collective/contact/content/widgets.py
@@ -2,16 +2,32 @@ from z3c.form.interfaces import IFieldWidget
 from z3c.form.widget import FieldWidget
 from zope.interface import implementer
 
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
+
 from plone.formwidget.autocomplete.widget import (
     AutocompleteMultiSelectionWidget,
     AutocompleteSelectionWidget)
 
 
-class ContactAutocompleteSelectionWidget(AutocompleteSelectionWidget):
+class ContactBaseWidget(object):
+    display_template = ViewPageTemplateFile('templates/contact_display.pt')
+    input_template = ViewPageTemplateFile('templates/contact_input.pt')
+
+    def js_extra(self):
+        return """
+$('.addcontact').prepOverlay({
+  subtype: 'ajax',
+  filter: common_content_filter,
+  formselector: 'form#addcontact'
+});
+
+"""
+
+class ContactAutocompleteSelectionWidget(ContactBaseWidget, AutocompleteSelectionWidget):
     autoFill = False
 
 
-class ContactAutocompleteMultiSelectionWidget(AutocompleteMultiSelectionWidget):
+class ContactAutocompleteMultiSelectionWidget(ContactBaseWidget, AutocompleteMultiSelectionWidget):
     autoFill = False
 
 
