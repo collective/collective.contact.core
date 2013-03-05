1b810b2fad56b4d80c0993a92dd81419e07593c6
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-05 17:29:50 +0100

    Hide tokens columns and update them (with a normalized string) when the name column change

--- src/collective/contact/core/browser/static/js/forms.js
+++ src/collective/contact/core/browser/static/js/forms.js
@@ -1,3 +1,4 @@
+/* Update person_title when gender changes (if person_title hasn't been set manually) */
 update_person_title = function(){
 	var gender = $(this).val();
 	var person_title = $('#form-widgets-person_title').val();
@@ -10,6 +11,50 @@ update_person_title = function(){
 	}
 }
 
+/* Replace spaces with underscores, removes accents and other special characters */
+function normalize_string(s) {
+	var rules = {
+		'a': /[àáâãäå]/g,
+		'ae': /[æ]/g,
+		'c': /[ç]/g,
+		'e': /[èéêë]/g,
+		'i': /[ìíîï]/g,
+		'n': /[ñ]/g,
+		'o': /[òóôõö]/g,
+		'oe': /[œ]/g,
+		'u': /[ùúûü]/g,
+		'y': /[ýÿ]/g,
+		'th': /[ðþ]/g,
+		'ss': /[ß]/g,
+		'_': /[\s\\]+/g
+	};
+	s = s.toLowerCase();
+	for (var r in rules) s = s.replace(rules[r], r);
+	return s;
+}
+
+/* Update the token which matches the input*/
+update_token = function(){
+	var name = $(this).val();
+	var token = $(this).closest('tr').find('input[id$="-widgets-token"]');
+	if (token.val() == '') {
+		token_value = normalize_string(name);
+		token.val(token_value);
+	}
+}
+
+/* Manage all stuff for the directory edit form */
+manage_directory = function(){
+    // hide some columns in directory edit form
+	$('input[id$="-widgets-token"]').hide();
+	$('#formfield-form-widgets-position_types thead').hide();
+	$('#formfield-form-widgets-organization_types thead').hide();
+	$('#formfield-form-widgets-organization_levels thead').hide();
+	// update tokens if necessary
+	$('input[id$="-widgets-name"]').blur(update_token);
+}
+
 $(document).ready(function(){
     $('#formfield-form-widgets-gender input').change(update_person_title);
+    manage_directory();
 });
