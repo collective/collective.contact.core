56842ae883c5e521bac102b6070b5aa0aa4bc3c7
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-06 12:23:08 +0100

    Directly use photo from context (instead of setting it on view)

--- src/collective/contact/core/browser/person.py
+++ src/collective/contact/core/browser/person.py
@@ -28,7 +28,6 @@ class Person(BaseView):
 
         self.person_title = person.person_title
         self.gender = person.gender or ''
-        self.photo = person.photo or ''
 
         catalog = getToolByName(self.context, 'portal_catalog')
         context_path = '/'.join(person.getPhysicalPath())
