b316dfedff5d153ba091da7a7fab0b8a1b4d6c3b
Cédric Messiant <cedricmessiant@ecreall.com>
2013-05-28 17:53:00 +0200

    Fix bug with directory view caused by the meta_type removal

--- src/collective/contact/core/browser/directory.py
+++ src/collective/contact/core/browser/directory.py
@@ -1,9 +1,15 @@
 from plone.dexterity.browser.view import DefaultView
+from plone import api
 
 
 class Directory(DefaultView):
 
     def update(self):
         super(Directory, self).update()
-        self.persons = self.context.objectValues('person')
-        self.organizations = self.context.objectValues('organization')
+        directory_path = '/'.join(self.context.getPhysicalPath())
+        search_path = {'query': directory_path, 'depth': 1}
+        catalog = api.portal.get_tool('portal_catalog')
+        self.persons = catalog.searchResults(portal_type="person",
+                                             path=search_path)
+        self.organizations = catalog.searchResults(portal_type="organization",
+                                                   path=search_path)
