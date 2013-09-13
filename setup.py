cfcf124ada7a3664600f941e1fdf63eae56a6a56
Cédric Messiant <cedricmessiant@ecreall.com>
2013-09-13 11:37:56 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (
     + '\n')
 
 setup(name='collective.contact.core',
-      version='1.0',
+      version='1.1.dev0',
       description="Core package for collective.contact add-ons",
       long_description=long_description,
       # Get more strings from
