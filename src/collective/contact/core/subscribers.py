9862a2ec879eb04e1899f1ac45bfcc54434aa054
Vincent Fretin <vincent.fretin@gmail.com>
2013-10-24 15:59:26 +0200

    don't reindex on ContainerModifiedEvent

--- src/collective/contact/core/subscribers.py
+++ src/collective/contact/core/subscribers.py
@@ -2,6 +2,7 @@ from five import grok
 
 from zope.lifecycleevent.interfaces import IObjectAddedEvent,\
     IObjectModifiedEvent
+from zope.container.contained import ContainerModifiedEvent
 
 from collective.contact.widget.interfaces import IContactContent
 from collective.contact.core.content.held_position import IHeldPosition
@@ -18,11 +19,17 @@ indexes_to_update = ['SearchableText']
 @grok.subscribe(IHeldPosition, IObjectAddedEvent)
 @grok.subscribe(IHeldPosition, IObjectModifiedEvent)
 def update_related_with_held_position(obj, event=None):
+    if isinstance(event, ContainerModifiedEvent):
+        return
+
     obj.get_person().reindexObject(idxs=indexes_to_update)
 
 
 @grok.subscribe(IPosition, IObjectModifiedEvent)
 def update_related_with_position(obj, event=None):
+    if isinstance(event, ContainerModifiedEvent):
+        return
+
     for held_position in obj.get_held_positions():
         held_position.reindexObject(idxs=indexes_to_update)
         update_related_with_held_position(held_position)
@@ -30,12 +37,18 @@ def update_related_with_position(obj, event=None):
 
 @grok.subscribe(IPerson, IObjectModifiedEvent)
 def update_related_with_person(obj, event=None):
+    if isinstance(event, ContainerModifiedEvent):
+        return
+
     for held_position in obj.get_held_positions():
         held_position.reindexObject(idxs=indexes_to_update)
 
 
 @grok.subscribe(IOrganization, IObjectModifiedEvent)
 def update_related_with_organization(obj, event=None):
+    if isinstance(event, ContainerModifiedEvent):
+        return
+
     for held_position in obj.get_held_positions():
         held_position.reindexObject(idxs=indexes_to_update)
         update_related_with_held_position(held_position)
