a6c8a2a6f3af6203f4afd2111d5f755640bafcd8
Thomas Desvenain <thomas.desvenain@gmail.com>
2013-11-20 10:31:05 +0100

    >=1.1 do not get 1.1.dev0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name='collective.contact.core',
       install_requires=[
           'ExtensionClass',
           'collective.z3cform.datagridfield',
-          'collective.contact.widget >= 1.1',
+          'collective.contact.widget > 1.0',
           'setuptools',
           'ecreall.helpers.upgrade',
           'five.grok',
