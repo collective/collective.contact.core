1f5216e81ecabf0653036fa460431670cec9c3ac
Cédric Messiant <cedricmessiant@ecreall.com>
2014-01-29 11:26:52 +0100

    Localize dates in heldpositions.pt

--- src/collective/contact/core/browser/person.py
+++ src/collective/contact/core/browser/person.py
@@ -5,6 +5,7 @@ from Products.CMFCore.utils import getToolByName
 from collective.contact.core.browser import TEMPLATES_DIR
 from collective.contact.core.browser.contactable import BaseView
 from collective.contact.core.content.person import IPerson
+from collective.contact.core.browser.utils import date_to_DateTime
 
 
 grok.templatedir(TEMPLATES_DIR)
@@ -34,11 +35,22 @@ class HeldPositions(grok.View):
             held_position = {}
             obj = brain.getObject()
             held_position['label'] = obj.label or obj.Title()
-            held_position['start_date'] = obj.start_date
-            held_position['end_date'] = obj.end_date
+            if obj.start_date is not None:
+                start_date = date_to_DateTime(obj.start_date)
+                held_position['start_date'] = person.toLocalizedTime(start_date)
+            else:
+                held_position['start_date'] = None
+
+            if obj.end_date is not None:
+                end_date = date_to_DateTime(obj.end_date)
+                held_position['end_date'] = person.toLocalizedTime(end_date)
+            else:
+                held_position['end_date'] = None
+
             #held_position['phone'] = obj.phone
             #held_position['email'] = obj.email
             held_position['object'] = obj
             held_position['organization'] =  obj.get_organization().get_root_organization()
             held_positions.append(held_position)
+
         self.held_positions = held_positions
