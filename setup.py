596db86ff00059138f80aa9b0fa0761cdcb91358
Cédric Messiant <cedricmessiant@ecreall.com>
2015-04-23 12:28:20 +0200

    We need collective.contact.widget > 1.2.2 to make label/prelabel customization effective

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name='collective.contact.core',
       install_requires=[
           'ExtensionClass',
           'collective.z3cform.datagridfield',
-          'collective.contact.widget > 1.0',
+          'collective.contact.widget > 1.2.2',
           'setuptools',
           'ecreall.helpers.upgrade >= 1.1.6.dev0',
           'five.grok',
