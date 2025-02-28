b08c2193de06b35a3b48439e6b7fe7d350bb11c0
Stephan Geulette <s.geulette@imio.be>
2025-02-28 11:02:54 +0100

    Back to development: 1.43.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (
 
 setup(
     name='collective.contact.core',
-    version='1.43.1',
+    version='1.43.2.dev0',
     description="Core package for collective.contact add-ons",
     long_description=long_description,
     # Get more strings from
